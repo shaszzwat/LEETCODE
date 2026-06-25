c0aecb3dac64e0d95edbca5a321da8f787f276d8
Shashwat karn <shashwatkarn200512@gmail.com>
2026-06-26 01:45:39 +0530

    Time: 99 ms (84.33%), Space: 182.7 MB (67.16%) - LeetHub

---
+++ LeetCode/Medium/2766-relocate-marbles/2766-relocate-marbles.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+        unordered_map<int,int>mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+        for(int i=0;i<moveTo.size();i++){
+            mpp[moveFrom[i]]=0;
+            if(mpp[moveFrom[i]]==0){
+                mpp.erase(moveFrom[i]);
+            }
+            mpp[moveTo[i]]++;
+        }
+        vector<int>ans;
+        for(auto it:mpp){
+            ans.push_back(it.first);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+
+        
+    }
+};
