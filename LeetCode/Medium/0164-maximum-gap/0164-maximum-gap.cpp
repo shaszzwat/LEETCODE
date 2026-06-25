7fc31e1763d7e83fc38284aaede4d2ae835252ef
Shashwat karn <shashwatkarn200512@gmail.com>
2026-06-25 18:35:03 +0530

    Time: 40 ms (57.42%), Space: 85.8 MB (76.12%) - LeetHub

---
+++ LeetCode/Medium/0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxi=0;
+        if(nums.size()<2){
+            return 0;
+        }
+        for(int i=1;i<nums.size();i++){
+            maxi=max(maxi,nums[i]-nums[i-1]);
+
+        }
+        return maxi;
+        
+    }
+};
