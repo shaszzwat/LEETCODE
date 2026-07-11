0374d2749a435e7a7c405179b2b778a6780569f9
Shashwat karn <shashwatkarn200512@gmail.com>
2026-07-11 13:49:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.63%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int mid=n;
+        
         int ini=1;
         int count=0;
         for(int i=0;i<n/2;i++){
-            count+=mid-ini;
+            count+=n-ini;
             ini=ini+2;
 
 
