48d1ab04b69c35b748c1ebfe1f5d57ae6c8ca8f7
Shashwat karn <shashwatkarn200512@gmail.com>
2026-07-12 18:17:02 +0530

    Time: 0 ms (100%), Space: 16.1 MB (83.92%) - LeetHub

---
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        stack<int> st;
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            if (st.empty() || arr[i] >= st.top()) {
+                st.push(arr[i]);
+            }
+            else {
+                int last = st.top();  
+                st.pop();
+
+                while (!st.empty() && st.top() > arr[i]) {
+                    st.pop();
+                }
+
+                st.push(last);
+            }
+        }
+
+        return st.size();
+    }
+};
